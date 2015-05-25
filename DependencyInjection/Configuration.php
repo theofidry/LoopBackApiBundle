26ed77cb0638b8845d66682a1264c121feb6af28
Théo FIDRY <theo.fidry@gmail.com>
2015-05-25 11:52:16 +0200

    First draft.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the LoopBackApiBundle package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fidry\LoopBackApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * The configuration of the bundle.
+ *
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('loopback_api');
+
+        $rootNode
+            ->children()
+                ->arrayNode('parameters')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('filter')->defaultValue('filter')->info('Keyword for the filter.')->end()
+                        ->scalarNode('search_filter')->defaultValue('where')->info('Keyword for the search filter.')->end()
+                        ->scalarNode('order_filter')->defaultValue('where')->info('Keyword for the search filter.')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ LoopBackApiBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the LoopBackApiBundle package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fidry\LoopBackApiBundle;
+
+use Dunglas\ApiBundle\DependencyInjection\Compiler\DataProviderPass;
+use Dunglas\ApiBundle\DependencyInjection\Compiler\ResourcePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * LoopBackApiBundle.
+ *
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+class LoopBackApiBundle extends Bundle
+{
+}
